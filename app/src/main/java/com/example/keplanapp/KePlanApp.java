4b8f375f496d68891bee5b679637a7d9c18d6f8f
carlosoracls <torresmorenojosecarlos@gmail.com>
2022-03-22 16:51:44 +0100

    Ultima version

--- app/src/main/java/com/example/keplanapp/KePlanApp.java
+++ app/src/main/java/com/example/keplanapp/KePlanApp.java
@@ -12,7 +12,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.keplanapp.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class KePlanApp extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
